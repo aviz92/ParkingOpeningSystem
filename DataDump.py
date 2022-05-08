ac02812398e105b5d6a2be3e29bdd85c6b89cb76
azaguri <avi210892@gmail.com>
2022-05-08 17:50:42 +0300

    ~

--- DataDump.py
+++ DataDump.py
@@ -1,39 +1,40 @@
 import os
-import datetime
+from datetime import datetime, timezone
 import time
 
-from InfrastructureSVG.Logger_Infrastructure.Projects_Logger import ProjectsLogging
+from Infrastructure.Logger_Infrastructure.Projects_Logger import ProjectsLogging
+from Infrastructure.Git_Infrastructure.GitAction_Infrastructure import GitActions
 
 
 def create_database_backup():
     try:
-        current_date_time = datetime.datetime.now().strftime("%Y-%m-%d__%H_%M")
-
+        current_date_time = datetime.now().strftime("%Y-%m-%d__%H_%M")
+        backup_database_name = f'backup_database\\{current_date_time}_datadump.json'
         os.system(
             f'cd {os.path.realpath(os.path.join(os.path.dirname(__file__)))} && '
             f'.\\venv\\Scripts\\activate && '
-            f'python manage.py dumpdata --natural-foreign --exclude=auth.permission --exclude=contenttypes --indent=4 > db.json'
+            f'python manage.py dumpdata --natural-foreign --exclude=auth.permission --exclude=contenttypes --indent=4 > {backup_database_name}'
         )
     except Exception as err:
         print(err)
 
-    # try:
-    #     logger.info('Create a new database backup1')
-    # except Exception as e:
-    #     print('Create a new database backup2')
-
 
 def main():
     try:
+        g = GitActions()
         while True:
             create_database_backup()
+            time.sleep(5)
+            # git_commit_and_push(string_in_file='_datadump')
+            g.git_add_commit_push()
+            # time.sleep(2)
             time.sleep(3600 * 2)
     except Exception as e:
         print(e)
 
 
 if __name__ == '__main__':
-    project_name = "DatabaseBackup"
+    project_name = "Create a database backup, commit and push"
     print('')
     print('####################################')
     print(f'### Start {project_name} Process ###')
@@ -43,7 +44,7 @@ if __name__ == '__main__':
 
     log_path = f'C:\\Python Logs\\{project_name}'
     log_file_name = f'{project_name}Logs'
-    logger = ProjectsLogging(project_name=project_name, path=log_path, file_name=log_file_name).project_logging()
+    logger = ProjectsLogging(project_name=project_name, path=log_path, file_name=log_file_name).project_logging(timestamp=True)
     logger.info(f'Start {project_name} Process')
     time.sleep(1)
 
---
+++ reinstall_packages.bat
@@ -0,0 +1,6 @@
+echo Install Infrastructure:
+pip uninstall -y Infrastructure
+pip install -r requirements.txt
+
+echo Install LDAP:
+pip install C:\Users\HomePC\PycharmProjects\aviz92\ParkingOpeningSystem\MoreFiles\LDAP\python_ldap-3.4.0-cp39-cp39-win_amd64.whl
